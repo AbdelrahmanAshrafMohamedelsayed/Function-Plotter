1b9510a587d9206858775f7a148b2fdf7f9280fd
AbdelrahmanAshrafMohamedelsayed <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2023-07-17 19:00:39 +0300

    Fix: TestFile

--- test_function_plotter.py
+++ test_function_plotter.py
@@ -11,14 +11,14 @@ def app(qtbot):
     qtbot.addWidget(plotter)
     return plotter
 
-def test_plot_function(qtbot, app):
+def test_end_to_end(qtbot, app):
     # Get references to the GUI widgets
     function_input = app.function_input
     min_input = app.min_input
     max_input = app.max_input
     plot_button = app.plot_button
     canvas = app.canvas
-    # Set input values
+    # Simulate user input
     qtbot.keyClicks(function_input, 'x^2')
     qtbot.keyClicks(min_input, '0')
     qtbot.keyClicks(max_input, '5')
@@ -26,7 +26,14 @@ def test_plot_function(qtbot, app):
     qtbot.mouseClick(plot_button, Qt.LeftButton)
     # Check if the plot is generated correctly
     assert len(canvas.figure.axes) == 1  # One plot is generated
-    assert len(canvas.figure.axes[0].lines) == 1  # One line is plotted one curve
+    assert len(canvas.figure.axes[0].lines) == 1  # One line is plotted
+    # Check the plotted line data
+    x_data = canvas.figure.axes[0].lines[0].get_xdata()
+    y_data = canvas.figure.axes[0].lines[0].get_ydata()
+    expected_x_data = [0, 1, 2, 3, 4, 5]
+    expected_y_data = [0, 1, 4, 9, 16, 25]
+    assert list(x_data) == expected_x_data
+    assert list(y_data) == expected_y_data
 
 def test_validate_input_valid_input(app):
     app.function_input.setText("x^2")
@@ -42,6 +49,12 @@ def test_validate_input_invalid_function_1(app):
 
 def test_validate_input_invalid_function_2(app):
     app.function_input.setText("3x")
+    app.min_input.setText("0")
+    app.max_input.setText("10")
+    assert app.validate_input() == False
+
+def test_validate_input_invalid_function_3(app):
+    app.function_input.setText("x**2")
     app.min_input.setText("0")
     app.max_input.setText("10")
     assert app.validate_input() == False
@@ -64,8 +77,3 @@ def test_validate_input_min_greater_than_max(app):
     app.max_input.setText("5")
     assert app.validate_input() == False
 
-def test_validate_input_invalid_function_3(app):
-    app.function_input.setText("x**2")
-    app.min_input.setText("0")
-    app.max_input.setText("10")
-    assert app.validate_input() == False
